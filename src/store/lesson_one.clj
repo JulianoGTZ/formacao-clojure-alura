c52fd720fe8ac54a6c993e2a2b89ff82602b1207
Juliano Lima <julianodgtz@gmail.com>
2020-01-23 16:22:14 -0300

    Finish my map implementation

--- src/store/lesson_one.clj
+++ src/store/lesson_one.clj
@@ -1,16 +1,16 @@
 (ns store.lesson-one)
 
-;; call my map
+;; call my map using Tail Recursion
 (defn my-map
   [function-to-apply elements]
-  (do
-    (let [val (first elements)]
-             (if (not (nil? val))
-               (do (function-to-apply val)
-                   (my-map function-to-apply (rest elements)))))))
+    (let [first-value (first elements)]
+             (if (not (nil? first-value))
+               (do
+                   (function-to-apply first-value)
+                   (recur function-to-apply (rest elements))))))
 
-(def elements [10 20 30 50])
-
+;; Function that will be passed to my-map
 (defn print-value [value] (println value))
 
-(my-map print-value elements)
+;; my-map call
+(my-map print-value (range 100000))
