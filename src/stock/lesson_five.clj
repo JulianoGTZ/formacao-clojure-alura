93c126e2c4b73a9832f2794c12d92cf52c78743b
Juliano Lima <julianodgtz@gmail.com>
2020-01-23 10:58:27 -0300

    Adds a lesson that is about Map, how acess maps and threading

---
+++ src/stock/lesson_five.clj
@@ -0,0 +1,12 @@
+(ns stock.lesson-five)
+
+;; Setting a map
+(def order {:bag { :amount 10 :price 10 }})
+
+
+;; Printing value
+(println "Order value" order)
+
+;; Threading example
+(println
+  (-> order :bag :amount))
