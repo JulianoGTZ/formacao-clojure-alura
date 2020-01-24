14b987bcb7441a56b67bb617421ab11a52845cea
Juliano Lima <julianodgtz@gmail.com>
2020-01-24 09:44:00 -0300

    Adds the override params

--- src/store/lesson_two.clj
+++ src/store/lesson_two.clj
@@ -1,6 +1,5 @@
 (ns store.lesson-two)
 
-
 ;; Function with override params
 (defn my-count
   ([elements]
@@ -10,9 +9,15 @@
    (if (seq elements)
      (recur (inc total-so-far) (rest elements))
      total-so-far))
-
   )
 
+(defn my-count-with-loop
+  [elements]
+  (loop [total-so-far 0 rest-elements elements]
+    (if (seq rest-elements) (recur (inc total-so-far) (next rest-elements)) total-so-far)))
 
-(println "First with one param"(my-count [2 3 4 5]))
-(println "And with two params"(my-count 10 [2 3 4 5]))
+
+
+(println "First with one param" (my-count [2 3 4 5]))
+(println "And with two params" (my-count 10 [2 3 4 5]))
+(println "And with my-count-with-loop" (my-count-with-loop [2 3 4 5]))
