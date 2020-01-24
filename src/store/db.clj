984456704e79be96f64bd067cf5040df6d1b0026
Juliano Lima <julianodgtz@gmail.com>
2020-01-24 10:24:19 -0300

    Update the db in memory

--- src/store/db.clj
+++ src/store/db.clj
@@ -1,45 +1,42 @@
 (ns store.db)
-(def order1 { :user 15
-             :items {:bag {:id :bag :amount 2 :unit-price 80}}
-             :items {:shirt {:id :shirt :amount 3 :unit-price 20}}
-             :items {:tennis {:id :tennis :amount 3}}
-             })
 
-(def order2 { :user 10
-             :items {:bag {:id :bag :amount 2 :unit-price 80}}
-             :items {:shirt {:id :shirt :amount 3 :unit-price 20}}
-             :items {:tennis {:id :tennis :amount 3}}
-             })
+(def order1 {:user 15
+              :items {
+                      :bag { :id :bag :amount 12 :unit-price 80}
+                      :shirt { :id :shirt :amount 44 :unit-price 45}
+                      :tennis { :id :tennis :amount 31}}})
 
-(def order3 { :user 11
-             :items {:bag {:id :bag :amount 2 :unit-price 80}}
-             :items {:shirt {:id :shirt :amount 3 :unit-price 20}}
-             :items {:tennis {:id :tennis :amount 3}}
-             })
+(def order2 {:user 16
+             :items {
+                     :bag { :id :bag :amount 33 :unit-price 80}
+                     :shirt { :id :shirt :amount 4 :unit-price 45}
+                     :tennis { :id :tennis :amount 21}}})
 
-(def order4 { :user 12
-             :items {:bag {:id :bag :amount 22 :unit-price 80}}
-             :items {:shirt {:id :shirt :amount 3 :unit-price 20}}
-             :items {:tennis {:id :tennis :amount 3}}
-             })
-
-(def order5 { :user 30
-             :items {:bag {:id :bag :amount 12 :unit-price 80}}
-             :items {:shirt {:id :shirt :amount 3 :unit-price 20}}
-             :items {:tennis {:id :tennis :amount 3}}
-             })
-
-(def order6 { :user 20
-             :items {:bag {:id :bag :amount 43 :unit-price 80}}
-             :items {:shirt {:id :shirt :amount 3 :unit-price 20}}
-             :items {:tennis {:id :tennis :amount 3}}
-             })
-
-(def order7 { :user 19
-             :items {:bag {:id :bag :amount 33 :unit-price 800}}
-             :items {:shirt {:id :shirt :amount 3 :unit-price 20}}
-             :items {:tennis {:id :tennis :amount 3}}
-             })
+(def order3 {:user 17
+             :items {
+                     :bag { :id :bag :amount 4 :unit-price 80}
+                     :shirt { :id :shirt :amount 4 :unit-price 45}
+                     :tennis { :id :tennis :amount 51}}})
+(def order4 {:user 19
+             :items {
+                     :bag { :id :bag :amount 22 :unit-price 80}
+                     :shirt { :id :shirt :amount 4 :unit-price 45}
+                     :tennis { :id :tennis :amount 66}}})
+(def order5 {:user 10
+             :items {
+                     :bag { :id :bag :amount 21 :unit-price 80}
+                     :shirt { :id :shirt :amount 4 :unit-price 45}
+                     :tennis { :id :tennis :amount 11}}})
+(def order6 {:user 21
+             :items {
+                     :bag { :id :bag :amount 12 :unit-price 80}
+                     :shirt { :id :shirt :amount 44 :unit-price 45}
+                     :tennis { :id :tennis :amount 10}}})
+(def order7 {:user 35
+             :items {
+                     :bag { :id :bag :amount 3 :unit-price 80}
+                     :shirt { :id :shirt :amount 5 :unit-price 45}
+                     :tennis { :id :tennis :amount 4}}})
 
 (defn all-orders []
   [order1 order2 order3 order4 order5 order6 order7])
