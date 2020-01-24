9618d5530eef9392ce3209c7de310383b86c4dd0
Juliano Lima <julianodgtz@gmail.com>
2020-01-24 14:14:41 -0300

    Adds the function that calculates the spent amount per user

--- src/store/lesson_three.clj
+++ src/store/lesson_three.clj
@@ -1,4 +1,35 @@
 (ns store.lesson-three
-  (:require [store.db :as db]))
+  (:require [store.db :as s.db]))
 
-(map (db/all-orders ))
+(defn total-item
+  [[_ details]]
+  (* (get details :amount 0) (get details :unit-price 0)))
+
+(defn total-order
+  [order]
+  (reduce + (map total-item order)))
+
+(defn total-order-value
+  [orders]
+  (->> orders
+       (map :items)
+       (map total-order)
+       (reduce +)))
+
+(defn amount-per-user
+  [[user orders]]
+  {
+    :user-id user
+    :total-of-orders (count orders)
+    :total-order-value (total-order-value orders)
+   })
+
+;; Get all orders
+;; Group by user key
+;; Get vals from key-map set
+;; Map count function
+;; Print the count
+(->> (s.db/all-orders)
+     (group-by :user)
+     (map amount-per-user)
+     println)
