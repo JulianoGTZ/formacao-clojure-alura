905b420a84e987c8643f44a6c9339bf09e2878a9
Juliano Lima <julianodgtz@gmail.com>
2020-01-31 13:58:36 -0300

    Create file to extract common logic between the stock components

---
+++ src/store/logic.clj
@@ -0,0 +1,31 @@
+(ns store.logic
+  )
+
+
+(defn total-item
+  [[_ details]]
+  (* (get details :amount 0) (get details :unit-price 0)))
+
+(defn total-order
+  [order]
+  (reduce + (map total-item order)))
+
+(defn total-order-value
+  [orders]
+  (->> orders
+       (map :items)
+       (map total-order)
+       (reduce +)))
+
+(defn amount-per-user
+  [[user orders]]
+  {
+   :user-id user
+   :total-of-orders (count orders)
+   :total-order-value (total-order-value orders)
+   })
+
+(defn resume-per-user [orders]
+  (->>())
+
+
