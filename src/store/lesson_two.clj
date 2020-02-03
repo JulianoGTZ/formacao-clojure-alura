0cc28b7f76faf1ebad761f94ca2b77963b5bad2d
Juliano Lima <julianodgtz@gmail.com>
2020-02-03 09:33:26 -0300

    Format lesson two

--- src/store/lesson_two.clj
+++ src/store/lesson_two.clj
@@ -16,8 +16,6 @@
   (loop [total-so-far 0 rest-elements elements]
     (if (seq rest-elements) (recur (inc total-so-far) (next rest-elements)) total-so-far)))
 
-
-
 (println "First with one param" (my-count [2 3 4 5]))
 (println "And with two params" (my-count 10 [2 3 4 5]))
 (println "And with my-count-with-loop" (my-count-with-loop [2 3 4 5]))
