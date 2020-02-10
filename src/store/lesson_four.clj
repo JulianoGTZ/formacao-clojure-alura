4cbe77437517089df94fb128e4b86dc563284d1f
Juliano Lima <julianodgtz@gmail.com>
2020-02-10 13:04:47 -0300

    Finish the lesson four

--- src/store/lesson_four.clj
+++ src/store/lesson_four.clj
@@ -1,4 +1,13 @@
 (ns store.lesson-four
-  (:require [store.db :as s.db]))
+  (:require [store.db :as s.db]
+             [store.logic :as s.logic]))
 
-(println (s.db/all-orders))
+;; Examples using Ordered and reverse functions
+(let [resume (s.logic/resume-per-user (s.db/all-orders))
+      orders (s.db/all-orders)]
+  (println resume)
+  (println "Ordered" (sort-by :total-price resume))
+  (println "Reverse" (reverse (sort-by :total-price resume)))
+
+  ;; Example with get-in
+  (println (get-in orders [0 :items :bag :amount])))
