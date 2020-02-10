de36b5f66d7bf34385f25c9da2038bda3f373b9c
Juliano Lima <julianodgtz@gmail.com>
2020-02-10 13:23:07 -0300

    Adds a funciton using some funciton  on lesson four

--- src/store/lesson_four.clj
+++ src/store/lesson_four.clj
@@ -9,10 +9,11 @@
   (println "Ordered" (sort-by :total-price resume))
   (println "Reverse" (reverse (sort-by :total-price resume)))
   (println (first resume))
-  (println (count resune))
+  (println (count resume))
   (println (get resume 1))
   (println (take 2 resume))
-
+  ;; Some example passing a predicate
+  (println "> 500 some =>" (some #(> (:total-price %)) resume))
 
   ;; Example with get-in
   (println (get-in orders [0 :items :bag :amount])))
