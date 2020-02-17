2d02c8a0a0910b618e9a1beb9d7096320b1b1d7a
Juliano Lima <julianodgtz@gmail.com>
2020-02-16 23:39:07 -0300

    Adds queue

--- src/hospital/collections.clj
+++ src/hospital/collections.clj
@@ -1,7 +1,17 @@
-(ns hospital.collections)
+(ns hospital.collections
+  (:use [clojure pprint]))
 
 (defn test-vector
   []
   (let [queue [111 222]]
     (println (conj queue 333))))
 
+(test-vector)
+
+(defn test-queue []
+  ;;Creating a queue
+  (let [queue (conj clojure.lang.PersistentQueue/EMPTY "111" "222")]
+  (println "Queue")
+  (pprint queue)))
+
+(test-queue)
