84b93f149b39b507941528a8508858504ce74226
Juliano Pereira Lima <julianodgtz@gmail.com>
2020-02-20 23:01:44 -0300

    Adds a shared function with exception-control

--- src/hospital/logic.clj
+++ src/hospital/logic.clj
@@ -1,8 +1,17 @@
 (ns hospital.logic)
 
+(defn hospital-is-full?
+  [hospital department]
+  (-> hospital
+      (get department)
+      count
+      (>= 5)))
+
 (defn arrives-in
   [hospital department people]
-  (update hospital department conj people))
+  (if (not (hospital-is-full? hospital department))
+    (update hospital department conj people)
+    (throw (ex-info "Hospital is full" {:try-add-people people}))))
 
 (defn attend
   [hospital department]
