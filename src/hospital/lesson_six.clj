dbe6672dd0a1806d17eb9157d26f70490ee01941
Juliano Pereira Lima <julianodgtz@gmail.com>
2020-06-11 21:16:19 -0300

    Create some functions using future

--- src/hospital/lesson_six.clj
+++ src/hospital/lesson_six.clj
@@ -2,9 +2,17 @@
   (:use     [clojure pprint])
   (:require [hospital.model :as h.model]))
 
+(defn fit_in_the_queue?
+  [queue]
+  (-> queue
+      count
+      (< 5)))
+
 (defn arrives-in
   [line person]
-  (conj line person))
+  (if (fit_in_the_queue? line)
+    (conj line person)
+    (throw (ex-info "Queue is already full" {:trying-add person}))))
 
 (defn arrives-in-by-refset-function!
   [hospital person]
@@ -23,7 +31,34 @@
                   :laboratory-2 (ref h.model/empty_queue)
                   :laboratory-3 (ref h.model/empty_queue)}]
     (dosync
-     (arrives-in-by-alter-function! hospital "Little John"))
+     (arrives-in-by-alter-function! hospital "Little John")
+     (arrives-in-by-alter-function! hospital "Little Mathis")
+     (arrives-in-by-alter-function! hospital "Little Jeremias")
+     (arrives-in-by-alter-function! hospital "Little Malaquias")
+     (arrives-in-by-alter-function! hospital "Little Marco")
+     (arrives-in-by-alter-function! hospital "Little George"))
     (pprint hospital)))
 
-(simulate-one-day)
+(defn async-arrives-in!
+  [hospital person]
+  (future
+    (Thread/sleep (rand 5000))
+    (dosync
+      (println "Trying to add this people" person)
+      (arrives-in-by-alter-function! hospital person))))
+
+(defn simulate-one-day-async!
+  []
+  (let [hospital {:queue        (ref h.model/empty_queue)
+                  :laboratory-1 (ref h.model/empty_queue)
+                  :laboratory-2 (ref h.model/empty_queue)
+                  :laboratory-3 (ref h.model/empty_queue)}]
+    (def futures (mapv #(async-arrives-in! hospital %) (range 10)))
+    (future
+      (dotimes [n 4]
+        (Thread/sleep 2000)
+        (pprint hospital)
+        (pprint futures)))))
+
+
+(simulate-one-day-async!)
