4d29bf668a407e4080385754975c30da871fcd26
Juliano Pereira Lima <julianodgtz@gmail.com>
2020-06-14 19:02:20 -0300

    Finish the lesson three

---
+++ src/records_and_protocols/lesson_three.clj
@@ -0,0 +1,31 @@
+(ns records-and-protocols.lesson-three
+  (:use clojure.pprint)
+  (:require [records-and-protocols.logic :as r.logic]))
+
+(defn load-patient
+  [id]
+  (println "Loading" id)
+  (Thread/sleep 1000)
+  {:id id :loaded-when (r.logic/now)})
+
+(defn load-when-is-not-cached
+  [cache id loader]
+  (if (contains? cache id)
+    cache
+    (let [patient (loader id)]
+      (assoc cache id patient))))
+
+(defprotocol Loadable
+  (load! [this id]))
+
+(defrecord Cache
+  [cache loader]
+  Loadable
+  (load! [this id]
+    (swap! cache load-when-is-not-cached id loader)
+    (get @cache id)))
+
+(def patients (->Cache (atom {}) load-patient))
+(pprint patients)
+(load! patients 15)
+(pprint patients)
