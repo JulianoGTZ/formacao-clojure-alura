5690bb7f06d15f960119956522d803f26c7b2376
Juliano Pereira Lima <julianodgtz@gmail.com>
2020-06-14 14:45:06 -0300

    Adds protocols stuff

---
+++ src/records_and_protocols/lesson_1.clj
@@ -0,0 +1,30 @@
+(ns records-and-protocols.lesson-1
+  (:use clojure.pprint))
+
+(defn add-patient
+  [patients patient]
+  (if-let [id (:id patient)]
+    (assoc patients id patient)
+    (throw (ex-info "Patient doesn't have an id" {:patient patient}))))
+
+(defn test-add-patients
+  []
+  (let [patients {}
+        john {:id 15 :name "John" :birth-date "18/09/1981"}
+        marry {:id 20 :name "Marry" :birth-date "18/09/1982"}
+        paul {:Name "Paul" :birth-date "18/10/1983"}]
+    (pprint (add-patient patients john))
+    (pprint (add-patient patients marry))
+    (pprint (add-patient patients paul))))
+
+#_(test-add-patients)
+
+(defrecord Patient [id name date-birth])
+
+(println (->Patient 15 "John" "18/05/2020"))
+(println (->Patient 15 "John" "31/05/2020"))
+(println (->Patient 15 "John" "16/05/2020"))
+(println (->Patient 15 "John" "28/05/2020"))
+
+(pprint (map->Patient {:id 15 :name "John" :birth-date "09/07/1992"}))
+(pprint (class (assoc (Patient. nil "Terry" "18/09/1981") :id 40)))
