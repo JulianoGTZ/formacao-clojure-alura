664e4910845d3059a266ae805b7ed30a59ec4dda
Juliano Pereira Lima <julianodgtz@gmail.com>
2020-09-14 20:38:51 -0300

    Adds unit test about the logic

---
+++ test/hospital/logic_test.clj
@@ -0,0 +1,15 @@
+(ns hospital.logic-test
+  (:require [clojure.test :refer :all]
+            [hospital.logic :refer :all]))
+
+(def empty_queue clojure.lang.PersistentQueue/EMPTY)
+
+(deftest on-full-hospital?
+  (testing "Should return false when the hospital is empty"
+    (is (not (full-hospital? {:queue empty_queue} :queue))))
+
+  (testing "Should return false when the hospital has space to someone"
+    (is (not (full-hospital? {:queue [1 2 3]} :queue))))
+
+  (testing "Should return true when hhe hospital is full"
+    (is (full-hospital? {:queue [ 1 2 3 4 5]} :queue))))
