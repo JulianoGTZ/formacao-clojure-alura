7942f5561b4e7fdc5cd14118dbe2cc9eaf4df125
Juliano Pereira Lima <julianodgtz@gmail.com>
2020-09-19 21:06:34 -0300

    Improve model

--- src/hospital/model.clj
+++ src/hospital/model.clj
@@ -1,4 +1,5 @@
-(ns hospital.model)
+(ns hospital.model
+  (:require [schema.core :as s]))
 
 (def empty_queue clojure.lang.PersistentQueue/EMPTY)
 
@@ -7,3 +8,11 @@
    :laboratory-1 empty_queue
    :laboratory-2 empty_queue
    :laboratory-3 empty_queue})
+
+(s/def Patient {:id s/Str})
+(s/def Department (s/queue Patient))
+(s/def Hospital {s/Keyword Department})
+
+(defn new-department
+  []
+  empty_queue)
